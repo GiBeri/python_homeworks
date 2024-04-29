3d63670e033cf36480668c14f5bc0a66c9f3fad2
GiBeri <giorgi.berishvili.3@btu.edu.ge>
2024-04-29 20:40:48 +0400

    completed the homework :)

--- car.py
+++ car.py
@@ -5,23 +5,25 @@ class Car:
         self.makeYear = makeYear
         self.fuelType = fuelType
         self.name = self.makeYear + self.model
-    
+
     def sell(self):
-        print(f'sold{self.name}')
+        print(f"sold{self.name}")
 
     def buy(self):
-        print(f'bought{self.name}')
+        print(f"bought{self.name}")
 
     def rent(self):
-        print(f'rented{self.name}')
+        print(f"rented{self.name}")
 
     def insurance(self):
-        print(f'insured{self.name}')
+        print(f"insured{self.name}")
+
 
 def main():
     obj1 = Car("Red", "Mercedes", 2015, "Gas")
     obj2 = Car("Blue", "BMW", 2013, "Gas")
     obj2 = Car("Orange", "Audi", 2009, "Diesel")
 
-if __name__ == __main__:
+
+if __name__ == "__main__":
     main()
