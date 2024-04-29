b4377856d3e6cc5a645be4f52b4a3eac3918767d
GiBeri <giorgi.berishvili.3@btu.edu.ge>
2024-04-29 19:06:53 +0400

    finished the homework

---
+++ car.py
@@ -0,0 +1,27 @@
+class Car:
+    def __init__(self, color: str, model: str, makeYear: int, fuelType: str) -> None:
+        self.color = color
+        self.model = model
+        self.makeYear = makeYear
+        self.fuelType = fuelType
+        self.name = self.makeYear + self.model
+    
+    def sell(self):
+        print(f'sold{self.name}')
+
+    def buy(self):
+        print(f'bought{self.name}')
+
+    def rent(self):
+        print(f'rented{self.name}')
+
+    def insurance(self):
+        print(f'insured{self.name}')
+
+def main():
+    obj1 = Car("Red", "Mercedes", 2015, "Gas")
+    obj2 = Car("Blue", "BMW", 2013, "Gas")
+    obj2 = Car("Orange", "Audi", 2009, "Diesel")
+
+if __name__ == __main__:
+    main()
