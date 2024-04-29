9b7676e58d10deae622a7edc7aa6ac95f6e5fa54
GiBeri <giorgi.berishvili.3@btu.edu.ge>
2024-04-29 18:45:33 +0400

    completed the homework

---
+++ rectangle.py
@@ -0,0 +1,21 @@
+class Rectangle:
+    def __init__(self, height: int, width: int, color: str) -> None:
+        self.height = height
+        self.width = width
+        self.color = color
+    
+    def get_perimeter(self):
+        return self.height * 2 + self.width * 2
+    
+    def get_area(self):
+        return self.height * self.width
+
+def main():
+    obj1 = Rectangle(1, 5, "blue")
+    obj2 = Rectangle(3, 3, "green")
+    obj3 = Rectangle(3, 7, "purple")
+
+    print(obj2.get_area())
+
+if __name__ == "__main__":
+    main()
