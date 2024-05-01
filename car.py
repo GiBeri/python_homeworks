8ee5f3a856c0b3b9952a23200dd0ff6e55029581
GiBeri <giorgi.berishvili.3@btu.edu.ge>
2024-05-01 16:05:20 +0400

    cleaned up

--- car.py
+++ car.py
@@ -1,10 +1,12 @@
 class Car:
-    def __init__(self, color: str, model: str, makeYear: int, fuelType: str) -> None:
+    """class for cars"""
+
+    def __init__(self, color: str, model: str, make_year: int, fuel_type: str) -> None:
         self.color = color
         self.model = model
-        self.makeYear = makeYear
-        self.fuelType = fuelType
-        self.name = self.makeYear + self.model
+        self.make_year = make_year
+        self.fuel_type = fuel_type
+        self.name = f"{self.make_year, self.model}"
 
     def sell(self):
         print(f"sold{self.name}")
@@ -22,7 +24,11 @@ class Car:
 def main():
     obj1 = Car("Red", "Mercedes", 2015, "Gas")
     obj2 = Car("Blue", "BMW", 2013, "Gas")
-    obj2 = Car("Orange", "Audi", 2009, "Diesel")
+    obj3 = Car("Orange", "Audi", 2009, "Diesel")
+
+    obj1.rent()
+    obj2.insurance()
+    obj3.buy()
 
 
 if __name__ == "__main__":
--- rectangle.py
+++ rectangle.py
@@ -1,21 +1,25 @@
 class Rectangle:
+    """class for rectangle"""
+
     def __init__(self, height: int, width: int, color: str) -> None:
         self.height = height
         self.width = width
         self.color = color
-    
+
     def get_perimeter(self):
         return self.height * 2 + self.width * 2
-    
+
     def get_area(self):
         return self.height * self.width
+
 
 def main():
     obj1 = Rectangle(1, 5, "blue")
     obj2 = Rectangle(3, 3, "green")
     obj3 = Rectangle(3, 7, "purple")
 
-    print(obj2.get_area())
+    print(obj1.get_area(), obj2.get_perimeter, obj3.get_area)
+
 
 if __name__ == "__main__":
     main()
