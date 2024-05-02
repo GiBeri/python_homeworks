f65da4ce824ff4b6b8f6370bfef21514afef83a3
GiBeri <giorgi.berishvili.3@btu.edu.ge>
2024-05-02 21:26:06 +0400

    .

---
+++ point2.py
@@ -0,0 +1,21 @@
+import math
+
+
+class Point:
+    """class for points"""
+
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+
+    def __str__(self) -> str:
+        return f"({self.x},{self.y})"
+
+    def find_point_distance(self, other: "Point"):
+        point_distance = math.sqrt((other.x - self.x) ** 2 + (other.y - self.y) ** 2)
+        return int(point_distance)
+
+
+a = Point(int(input("enter x value for a: ")), int(input("enter y value for a: ")))
+b = Point(int(input("enter x value for b: ")), int(input("enter y value for b: ")))
+print(a.find_point_distance(b))
