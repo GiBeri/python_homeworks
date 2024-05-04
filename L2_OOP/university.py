0623cf8575ab51593fb1cea762514e3718d066a9
GiBeri <giorgi.berishvili.3@btu.edu.ge>
2024-05-05 02:07:14 +0400

    completed the homework :))

---
+++ L2_OOP/university.py
@@ -0,0 +1,38 @@
+class People:
+    def __init__(self, firstname: str, lastname: str) -> None:
+        self.firstname = firstname
+        self.lastname = lastname
+
+    def get_email(self) -> str:
+        return f"{self.firstname}.{self.lastname}.uni@btu.edu.ge"
+
+
+class Lecturer(People):
+    def __init__(self, firstname: str, lastname: str, salary: int) -> None:
+        super().__init__(firstname, lastname)
+        self.salary = salary
+
+    def get_email(self) -> str:
+        return f"{self.firstname}.{self.lastname}@btu.edu.ge"
+
+
+class Student(People):
+    def __init__(self, firstname: str, lastname: str, Courses: list[str] = []) -> None:
+        super().__init__(firstname, lastname)
+        self.Courses = Courses
+
+    def get_email(self) -> str:
+        return f"{self.firstname}.{self.lastname}.1@btu.edu.ge"
+
+
+def main():
+    s1 = Student("Giorgi", "Berishvili")
+    l1 = Lecturer("Lika", "Svanidze", 5000)
+    p1 = People("Leonardo", "Dicaprio")
+    print(s1.get_email())
+    print(l1.get_email())
+    print(p1.get_email())
+
+
+if __name__ == "__main__":
+    main()
